b97818d1736582301531e33fd8a11ea7d02d76cc
otecdima <93607175+otecdima@users.noreply.github.com>
2023-01-05 02:28:04 +0200

    Commit

--- src/main/java/decorator/CachedDocument.java
+++ src/main/java/decorator/CachedDocument.java
@@ -1,7 +1,31 @@
 package decorator;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor @Getter @Setter
 public class CachedDocument {
-    private Document document;
+    private int id;
+    private String gcsPath;
+    private String parsed;
 
+    public void save() {
+        DBConnection dbConnection = DBConnection.getInstance();
+        String query = String.format("insert into cache_table (gcsPath, parsed) values ('%s', '%s');",
+                gcsPath, parsed.replace('\'', '\"'));
+        dbConnection.executeQuery(query);
+    }
 
+    public int check() {
+        DBConnection dbConnection = DBConnection.getInstance();
+        String query = String.format("select count(*) from cache_table where gcsPath=('%s');", gcsPath);
+        return dbConnection.checking(query);
+    }
+
+    public String get() {
+        DBConnection dbConnection = DBConnection.getInstance();
+        String query = String.format("select * from cache_table where gcsPath=('%s');", gcsPath);
+        return dbConnection.getting(query);
+    }
 }
